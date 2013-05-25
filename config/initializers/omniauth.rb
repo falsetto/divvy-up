15e592119a1264bbfe6068abc8b117b864ec8b7b
Jedidiah Hurt <jhurt@kickbackpoints.com>
2013-05-24 18:43:28 -0600

    Add e2e_test environment config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,3 @@
-if Rails.env == 'e2e-test'
-  OmniAuth.config.test_mode = true
-  ENV['TWITTER_KEY'] = 'shiny'
-  ENV['TWITTER_SECRET'] = 'shhhhh'
-end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
